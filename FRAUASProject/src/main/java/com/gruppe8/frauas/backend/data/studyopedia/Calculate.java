955668ed367c20bb76e11b18c3d41933a357987c
dragonlp71 <dragonlp71@gmail.com>
2024-01-12 15:47:22 +0100

    Daljits code

---
+++ FRAUASProject/src/main/java/com/gruppe8/frauas/backend/data/studyopedia/Calculate.java
@@ -0,0 +1,15 @@
+package studyopedia;
+import javax.swing.JOptionPane;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import java.util.List;
+
+public class Calculate {
+	public static void main (String[] args) {
+	//DroneDataProcess.CreateGUI();
+	
+	//Either use buttons or filter for ASC/DESC order for attributes 
+	String searchattribute = JOptionPane.showInputDialog("By what attribute do you want to sort: ");
+	
+	}
+}
